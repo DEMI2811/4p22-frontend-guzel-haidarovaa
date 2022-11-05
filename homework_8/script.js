a6b8ab0e116bba8d77a139c8e41bdcbce7e15cbc
Guzel <haidarova-g@mail.ru>
2022-11-05 20:26:28 +0300

    add homework_8

--- homework_8/script.js
+++ homework_8/script.js
@@ -1,32 +1,86 @@
 'use strict';
 
-const out = document.getElementById('out');
 const button = document.getElementById('submit');
+const resultform = document.getElementById('resultform');
 
-
-
+//button.addEventListener('click', (event) => {
+  
 function func() {
+const num1 = document.getElementById('num1').value;
+const operator = document.getElementById('operator').value;
+const num2 = document.getElementById('num2').value;
+let result;
 
-    const number1 = document.getElementById('number1').value;
-    const operator = document.getElementById('operator').value;
-    const number2 = document.getElementById('number2').value;
-    let result;
 
-    switch (operator) {
-        case '+':
-            result = Number(number1) + Number(number2);
-            break;
-        case '-':
-            result = Number(number1) - Number(number2);
-            break; 
-        case '/':
-            result = Number(number1) / Number(number2);
-        break;  
-        case '*':
-            result = Number(number1) * Number(number2);
+   // event.preventDefault();
+
+
+
+    if (num1 === "") {
+        resultform.innerText = "Первое число не указано";
+        return console.log("Первое число не указано");
+      }
+    
+      if (num2 === "") {
+        resultform.innerText = "Второе число не указано";
+        return console.log("Второе число не указано");
+      }
+
+      if (num1 === " ") {
+        resultform.innerText = "Первое число не указано";
+        return console.log("Первое число не указано");
+      }
+    
+      if (num2 === " ") {
+        resultform.innerText = "Второе число не указано";
+        return console.log("Второе число не указано");
+      }
+    
+      if (isNaN(num1) === true || isNaN(num2) === true) {
+        resultform.innerText = "Некорректный ввод чисел";
+        return console.log("Некорректный ввод чисел");
+      }
+
+      if (operator === "") {
+        resultform.innerText = "Не введён знак";
+        return console.log("Не введён знак");
+      }
+     
+switch (operator) {
+    case "":
+      return console.log("Не введён знак");
         break;
+    case '+':
+        result = Number(num1) + Number(num2);
+      break;
+    case '-':
+        result = Number(num1) - Number(num2);
+      break;
+    case '/':
+        result = Number(num1) / Number(num2);
+      break;
+    case '*':
+        result = Number(num1) * Number(num2);
+     break;
+    default:
+      return  resultform.innerText = "Программа не поддерживает такую операцию";
+      console.log("Программа не поддерживает такую операцию");
+           break;
+ }
 
-    }
+      if (isNaN(result)) {
+        resultform.innerText = "Некорректный ввод чисел";
+        return console.log("Некорректный ввод чисел");
+      }
 
-    console.log(result)
+      if (result === -Infinity || result === Infinity) {
+        resultform.innerText = "Операция некорректна";
+        console.log("Операция некорректна");
+      } 
+      
+      else {
+        resultform.innerText = result;
+        console.log(result);
+      }
+//})
 }
